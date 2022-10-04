5a8c2b61cbff3e576664c0252271088ffcbd695c
Guilherme Rossa <guilherme.rossa@unidavi.edu.br>
2022-10-04 15:43:46 -0300

    Meu_Ecommerce
    Esse projeto foi desenvolvido no curso DEV-TI, junto o professor Marcondes Maçaneiro.

---
+++ home.php
@@ -0,0 +1,79 @@
+<div class="fundo">
+    <div class="topo">
+                <div class="topo1">
+                    <strong>Bem-vindo</strong>
+                </div>  
+    </div>
+            <div class="navbar">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Contato</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Quem Somos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Carrinho</a>
+                        </li>
+                    </ul>
+            </div>
+            <div class="pesquisar">
+                    <ul class="pesquisa" role="search">
+                        <input class="barrapesquisa" type="search" placeholder="Pesquisar Produto">
+                        <button type="btn" type="submit">?</button>
+                    </ul>
+            </div>
+            <div class="Barra">
+                <div class="barraanuncio">
+                    <a>10% De Desconto no PIx e no Boleto!</a>
+                </div>
+            </div>
+            <div class="categoria"><a>CATEGORIAS</a>
+                <br>
+                <?php 
+                include 'menu_categorias.php'; 
+                ?>
+            </div>
+
+            <div class="card1" style="width: 18rem;">
+                <img src="iphone-13.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">iphone 13</h5>
+                    <p class="card-text">HDR
+                                        True Tone
+                                        Ampla tonalidade de cores (P3)
+                                        Resposta tátil
+                                        Proporção de contraste: 2.000.000:1 (típica)
+                                        Brilho máx. de 800 nits (típico); brilho máx. de 1200 nits (HDR)
+                                        Revestimento resistente a impressões digitais e oleosidade
+                                        Suporte à exibição simultânea de vários idiomas e caracteres
+                                        R$ 10.000,00
+                    </p>
+                    <a href="produto_detalhe.php" class="btn btn-primary">Ver mais</a>
+                </div>
+            </div>
+            <div class="card2" style="width: 18rem;">
+                <img src="fone.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Fone</h5>
+                    <p class="card-text">fone ruim, bateria ruim, audio ruim, construcao ruim.
+                        R$ 10.000,00
+                    </p>
+                    <a href="#" class="btn btn-primary">Ver mais</a>
+                </div>
+            </div>
+            <div class="card3" style="width: 18rem;">
+                <img src="fone.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Fone</h5>
+                    <p class="card-text">fone ruim, bateria ruim, audio ruim, construcao ruim.
+                        R$ 10.000,00
+                    </p>
+                    <a href="#" class="btn btn-primary">Ver mais</a>
+                </div>
+            </div>
+
+</div>
